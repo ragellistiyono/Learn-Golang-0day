eeca41746e4b4040a7e44073f7a49c6d8e9126cd
humblegod <ragel@it.student.pens.ac.id>
2024-07-19 21:27:44 +0700

    Updated History of me Learning Golang

--- variable.go
+++ variable.go
@@ -14,4 +14,11 @@ func main() {
 	usia := "26"
 
 	fmt.Printf("Saya Berasal dari %s dan berumur %s ", asalKota, usia)
+	fmt.Println()
+
+	//Deklarasi multi Variabel
+
+	hobby, status, inspired := "Badminton", "Mahasiswa", "Lionel Messi"
+
+	fmt.Printf("Hobby saya %s, status saya saat ini adalah %s, inspirasi saya %s", hobby, status, inspired)
 }
