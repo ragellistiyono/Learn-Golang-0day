5b9d932bb513b27a386e1859e9da68c503c39b44
humblegod <ragel@it.student.pens.ac.id>
2024-07-20 17:01:28 +0700

    Updated History of me Learning Golang

--- Operator.go
+++ Operator.go
@@ -7,7 +7,6 @@ func main() {
 	hitungan1 := (((2+3)*2)*8 + 2) / 2
 
 	fmt.Printf("hasilnya adalah %d\n", hitungan1)
-	fmt.Printf("===============================\n")
 
 	//Operator Perbandingan
 	angka := 10*10 + 10
@@ -15,7 +14,9 @@ func main() {
 	hasilAngka1 := (angka < 120)
 	hasilAngka2 := (angka > 120)
 
-	fmt.Printf("Operator perbandingan dari 10*10+10\n")
+	fmt.Printf("=======================================\n")
+	fmt.Printf("= Operator perbandingan dari 10*10+10 =\n")
+	fmt.Printf("=======================================\n")
 	fmt.Printf("Apakah hasilnya sama dengan %d (%t)\n", angka, hasilAngka)
 	fmt.Printf("Apakah hasil lebih besar dari %d (%t)\n", angka, hasilAngka2)
 	fmt.Printf("Apakah hasil kurang dari %d (%t)\n", angka, hasilAngka1)
