cffa2cd47277e6a4e82ec902e1deedd890549f7e
humblegod <ragel@it.student.pens.ac.id>
2024-07-25 04:04:40 +0700

    Updated History of me Learning Golang

---
+++ Array.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Array Vertikal
+	var nama = [4]string{"ragel", "messi", "ralisto", "reddington"}
+
+	//Array Horizontal
+	var olahraga = [2]string{
+		"bola",
+		"badminton",
+	}
+
+	//Array dengan gaya type inference dan tipe data campur menggunakan inteface
+	campuran := [4]interface{}{"Ragel", 10, "Listiyono", 19.27}
+
+	//Array tanpa jumlah elemen
+	angka := [...]int{1, 2, 3, 4}
+
+	//Array Multidimensi
+	multiDimensi := [2][4]string{
+		{
+			"Ragel",
+			"Listiyono",
+			"10",
+			"September"},
+		{
+			"Surabaya",
+			"Indonesia",
+			"Laki-Laki",
+			"Mahasiswa"}}
+
+	fmt.Println("Nama :\t\t", nama)
+	fmt.Println("Olahraga:\t", olahraga)
+	fmt.Println("Data Campur:\t", campuran)
+	fmt.Println("Angka: \t\t", angka)
+	fmt.Println("Array Multi:\t", multiDimensi)
+}
